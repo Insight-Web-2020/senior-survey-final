cf4058b006c11e1aa996d064e4304f81248c8b10
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-08-31 03:26:05 +0530

    added lifestyle page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {HomeComponent} from './home/home.component';
 import {ContentComponent} from './content/content.component';
 import {CareerComponent} from './career/career.component';
 import {AcademicsComponent} from './academics/academics.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { PersonalComponent } from './personal/personal.component';
+import { ExtracurricularsComponent } from './extracurriculars/extracurriculars.component';
+import { OpinionsComponent } from './opinions/opinions.component';
 
 const routes: Routes = [
 	{path:'',component:HomeComponent,data:{animation:'homepage'}},
@@ -13,10 +17,10 @@ const routes: Routes = [
 		children:[
 			{path:'career',component:CareerComponent,data:{animation:'careerpage'}},
 			{path:'academics',component:AcademicsComponent,data:{animation:'academicspage'}},
-			{path:'lifestyle',component:AcademicsComponent,data:{animation:'lifestylepage'}},
-			{path:'personal',component:AcademicsComponent,data:{animation:'personalpage'}},
-			{path:'extracurricular',component:AcademicsComponent,data:{animation:'extracurricularpage'}},
-			{path:'opinions',component:AcademicsComponent,data:{animation:'opinionspage'}},
+			{path:'lifestyle',component:LifestyleComponent,data:{animation:'lifestylepage'}},
+			{path:'personal',component:PersonalComponent,data:{animation:'personalpage'}},
+			{path:'extracurricular',component:ExtracurricularsComponent,data:{animation:'extracurricularpage'}},
+			{path:'opinions',component:OpinionsComponent,data:{animation:'opinionspage'}},
 		]
 	},
 ];
--- src/app/lifestyle/lifestyle.component.ts
+++ src/app/lifestyle/lifestyle.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lifestyle',
   templateUrl: './lifestyle.component.html',
-  styleUrls: ['./lifestyle.component.scss']
+  styleUrls: ['./lifestyle.component.scss',
+  'contentstyles.css'
+  ]
 })
 export class LifestyleComponent implements OnInit {
 
